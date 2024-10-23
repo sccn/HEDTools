95eb757f26dd6b64212e91262af19a8d693ba55a
Dung Truong <dt.young112@gmail.com>
2024-10-23 14:37:54 -0400

    fix bugs. Update version

--- helpers/utilities/removeTagsEEG.m
+++ helpers/utilities/removeTagsEEG.m
@@ -1,25 +1,9 @@
-function [EEG,fMap] = removeTagsEEG(EEG)
-    fMap = [];
+function [EEG,tags] = removeTagsEEG(EEG)
     fprintf('Clearing EEG tags... ');
-    if hasSummaryTags(EEG)
-        fMap = fieldMap.createfMapFromStruct(EEG.etc.tags);
-        EEG.etc = rmfield(EEG.etc, 'tags');
+    tags = '';
+    if isfield(EEG.etc, 'HED')
+        tags = EEG.etc.HED;
+        EEG.etc = rmfield(EEG.etc, 'HED');
     end
-    if isfield(EEG.event, 'HED')
-        fMap = findtags(EEG);
-        EEG = pop_editeventfield(EEG, 'HED', []);
-    end
-
-    function summaryFound = hasSummaryTags(EEG)
-        % Returns true if there are summary tags found in the .etc field
-        summaryFound = isfield(EEG, 'etc') && ...
-            isstruct(EEG.etc) && ...
-            isfield(EEG.etc, 'tags') && ...
-            isstruct(EEG.etc.tags) && ...
-            isfield(EEG.etc.tags, 'map') && ...
-            isstruct(EEG.etc.tags.map) && ...
-            isfield(EEG.etc.tags.map, 'field');
-    end % hasSummaryTags
-    
 
 end % removeTagsEEG
--- helpers/utilities/removeTagsSTUDY.m
+++ helpers/utilities/removeTagsSTUDY.m
@@ -1,27 +1,15 @@
-function [STUDY,EEG,fMap] = removeTagsSTUDY(STUDY,EEG)
+function [STUDY,EEG,tags] = removeTagsSTUDY(STUDY,EEG)
     fprintf('Clearing STUDY tags... \n');
-    fMap = fieldMap();
-    if hasSummaryTags(STUDY)
-        fMap = fieldMap.createfMapFromStruct(STUDY.etc.tags);
-        STUDY.etc = rmfield(STUDY.etc, 'tags');
+    tags = '';
+    if isfield(STUDY.etc, 'HED')
+        tags = STUDY.etc.HED;
+        STUDY.etc = rmfield(STUDY.etc, 'HED');
     end
     for i=1:length(EEG)
         EEGTemp = EEG(i);
-        [EEGTemp, fMapEEG] = removeTagsEEG(EEGTemp);
+        [EEGTemp, ~] = removeTagsEEG(EEGTemp);
         EEG(i) = EEGTemp;
-        fMap.merge(fMapEEG, 'Merge', {},{});
         pop_saveset(EEG(i), 'filename', EEG(i).filename, 'filepath', EEG(i).filepath); 
     end    
-    fprintf('Done.\n')
-    function summaryFound = hasSummaryTags(STUDY)
-        % Returns true if there are summary tags found in the .etc field
-        summaryFound = isfield(STUDY, 'etc') && ...
-            isstruct(STUDY.etc) && ...
-            isfield(STUDY.etc, 'tags') && ...
-            isstruct(STUDY.etc.tags) && ...
-            isfield(STUDY.etc.tags, 'map') && ...
-            isstruct(STUDY.etc.tags.map) && ...
-            isfield(STUDY.etc.tags.map, 'field');
-    end % hasSummaryTags
-    
+    fprintf('Done.\n')    
 end % removeTagsSTUDY
